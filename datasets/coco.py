e4280226f06389efbfd2fb1a0eb757f401fe4722
Sudarshini Tyagi <tyagi.sudarshini@gmail.com>
2020-04-14 18:55:25 -0400

    Adding new data loader for Breast Dataset

--- datasets/coco.py
+++ datasets/coco.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import random
 import csv
+import h5py
 
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
@@ -18,6 +19,8 @@ import skimage.color
 import skimage
 import cv2
 from PIL import Image
+import torch
+import torchvision
 
 
 class CocoDataset(Dataset):
@@ -122,6 +125,105 @@ class CocoDataset(Dataset):
     def num_classes(self):
         return 80
 
+
+def read_image_mat(file_name):
+    data = h5py.File(file_name, "r")
+    image = np.array(data["image"]).T
+    data.close()
+    return image
+
+class CustomCocoDetection(torchvision.datasets.coco.CocoDetection):
+    """
+    Custom version of Coco dataset that loads from hdf5 instead
+    """
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: Tuple (image, target). target is the object returned by ``coco.loadAnns``.
+        """
+        coco = self.coco
+        img_id = self.ids[index]
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        target = coco.loadAnns(ann_ids)
+
+        path = coco.loadImgs(img_id)[0]["file_name"]
+
+        image_level = coco.loadImgs(img_id)[0].get("image_level", 0)
+
+        img = Image.fromarray(
+            read_image_mat(os.path.join(self.root, path)) / 16
+        ).convert("RGB")
+        best_center = coco.loadImgs(img_id)[0].get("best_center", (0, 0))
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+
+class BreastDataset(CustomCocoDetection):
+    def __init__(self, ann_file=None, root=None, set_type="train", transforms=None):
+        if set_type == "train":
+            root = "/gpfs/data/geraslab/jp4989/data/2010_2017_cropped_images_hdf5/"
+            ann_file = "breasts/train_with_unknowns.json"
+        elif set_type == "test":
+            root = "/gpfs/data/geraslab/jp4989/data/2010_2017_cropped_images_hdf5/",
+            ann_file = "breasts/test_with_unknowns.json"
+        elif set_type == "val":
+            root = "/gpfs/data/geraslab/jp4989/data/2010_2017_cropped_images_hdf5/",
+            ann_file = "breasts/val_with_unknowns.json"
+        super(BreastDataset, self).__init__(root, ann_file)
+        self.biopsy_image_ratio = float(os.environ.get("BIOPSY_RATIO", 0.5))
+        self.max_annotation_size = int(os.environ.get("MAX_ANNOTATION_SIZE", 0))
+        self.max_biopsy = 13444
+        self.split = ann_file.split("/")[-1].split(".")[
+            0
+        ]  # Ugly hack to workaround expected format
+        self.split = self.split.replace("_with_unknowns", "")
+
+        self.ids = sorted(self.ids)
+
+        self.json_category_id_to_contiguous_id = {
+            v: i + 1 for i, v in enumerate(self.coco.getCatIds())
+        }
+        self.contiguous_category_id_to_json_id = {
+            v: k for k, v in self.json_category_id_to_contiguous_id.items()
+        }
+        self.id_to_img_map = {k: v for k, v in enumerate(self.ids)}
+
+        self.transforms = transforms
+
+    def __getitem__(self, idx):
+        if (
+            self.split == "train"
+            and idx > self.max_biopsy
+            and random.random() < self.biopsy_image_ratio
+        ):
+            idx = random.randrange(self.max_biopsy)
+
+        img, anno = super(BreastDataset, self).__getitem__(
+            idx
+        )
+
+        if self.max_annotation_size and self.split == "train":
+            anno = [a for a in anno if a["bbox"][2] * a["bbox"][3] <= self.max_annotation_size]
+
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, anno)
+
+        return img, anno
+
+    def get_img_info(self, idx):
+        img_id = self.id_to_img_map[idx]
+        img_data = self.coco.imgs[img_id]
+        return img_data
 
 if __name__ == '__main__':
     from augmentation import get_augumentation
